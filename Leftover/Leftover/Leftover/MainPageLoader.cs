3cf66e6bf8d49da5ca84efc6ffc0678e7c9c4794
Mathias-Desktop <mathias67@hotmail.dk>
2017-11-18 18:13:32 +0100

    Login og flyttet silv kode

--- Leftover/Leftover/Leftover/MainPageLoader.cs
+++ Leftover/Leftover/Leftover/MainPageLoader.cs
@@ -11,117 +11,21 @@ namespace Leftover
 {
     public partial class MainPageLoader : ContentPage
     {
-
-        static Image imageFirst;
-
-
+        
         public MainPageLoader()
         {
             InitializeComponent();
-            var page = new NavigationPage();
-            
-            NavigationPage.SetTitleIcon(page, "FoodImage1.jpg");
-
-
-            
-           
-            var toolbar1 = new ToolbarItem
-            {
-                Text = "item 1 ",
-
-
-
-            };
-
-            this.ToolbarItems.Add(toolbar1);
-
-
-
-            //this.ToolbarItems.Add(new ToolbarItem("item 1","", () => { }));
-
-
-
-            Label label = new Label
-            {
-
-                Text = "Leftovers",
-
-                Font = Font.SystemFontOfSize(NamedSize.Large),
-
-                //FontAttributes = FontAttributes.Bold,
-                HorizontalOptions = LayoutOptions.Center
-
-            };
-
-            Label imageFirstLabel = new Label
-            {
-
-                Text = "Leftovers",
-                Font = Font.SystemFontOfSize(NamedSize.Medium),
-                //FontAttributes = FontAttributes.Bold,
-                HorizontalOptions = LayoutOptions.Center
-
-            };
-
-
-            imageFirst = new Image
-            {
-                Source = ImageSource.FromFile("FoodImage1.jpg"),
-                TranslationY = 20
-
-            };
-            imageFirst.GestureRecognizers.Add(new TapGestureRecognizer
-            {
-                Command = new Command(() => ImageFirstTapped())
-
-            });
-
-
-
-            Image imageSecond = new Image
-            {
-
-                Source = ImageSource.FromFile("FoodImage1.jpg"),
-
-                //TranslationY = imageFirst.TranslationY + 10,
-            };
-
-            imageSecond.GestureRecognizers.Add(new TapGestureRecognizer
-            {
-                Command = new Command(() => ImageSecondTapped())
-            });
-
-
-            this.Content = new StackLayout
-            {
-                Children =
-                {
-
-                imageFirst,
-                imageSecond
-                }
-            };
-
-
-
-
         }
 
-        void ImageFirstTapped()
+        void LoginClick(object sender, EventArgs e)
         {
-
-            Navigation.PushModalAsync(new Food());
+            if(UsernameEntry.Text == "Admin" && PasswordEntry.Text == "Admin")
+            {
+                Navigation.PushModalAsync(new Shop());
+            }
         }
 
-        void ImageSecondTapped()
-        {
-            Navigation.PushModalAsync(new Food());
-        }
-        void Settingsbutton()
-        {
-
-            Navigation.PushModalAsync(new Food());
-        }
+       
 
     }
 }
---
+++ Leftover/Leftover/Leftover/Shop.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Leftover
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Shop : ContentPage
+	{
+
+        static Image imageFirst;
+
+        public Shop ()
+		{
+			InitializeComponent ();
+
+            var page = new NavigationPage();
+
+            NavigationPage.SetTitleIcon(page, "FoodImage1.jpg");
+
+            var toolbar1 = new ToolbarItem
+            {
+                Text = "item 1 ",
+
+
+
+            };
+
+            this.ToolbarItems.Add(toolbar1);
+
+
+
+            //this.ToolbarItems.Add(new ToolbarItem("item 1","", () => { }));
+
+
+
+            Label label = new Label
+            {
+
+                Text = "Leftovers",
+
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+
+                //FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
+
+            };
+
+            Label imageFirstLabel = new Label
+            {
+
+                Text = "Leftovers",
+                Font = Font.SystemFontOfSize(NamedSize.Medium),
+                //FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
+
+            };
+
+
+            imageFirst = new Image
+            {
+                Source = ImageSource.FromFile("FoodImage1.jpg"),
+                TranslationY = 20
+
+            };
+            imageFirst.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => ImageFirstTapped())
+
+            });
+
+
+
+            Image imageSecond = new Image
+            {
+
+                Source = ImageSource.FromFile("FoodImage1.jpg"),
+
+                //TranslationY = imageFirst.TranslationY + 10,
+            };
+
+            imageSecond.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(() => ImageSecondTapped())
+            });
+
+
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+
+                imageFirst,
+                imageSecond
+                }
+            };
+
+        }
+
+        void ImageFirstTapped()
+        {
+
+            Navigation.PushModalAsync(new Food());
+        }
+
+        void ImageSecondTapped()
+        {
+            Navigation.PushModalAsync(new Food());
+        }
+        void Settingsbutton()
+        {
+
+            Navigation.PushModalAsync(new Food());
+        }
+    }
+}
